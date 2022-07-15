492a19f93d9a34d1d0ad2341962d60a1067e04ef
Saloni Jindal <salonijindal@MacBook-Pro.ht.home>
2022-07-15 03:16:54 -0400

    Get video from /:videoID API is working

--- routes/videos.js
+++ routes/videos.js
@@ -3,17 +3,31 @@ const router = express.Router();
 const fs = require("fs");
 const { v4: uuid } = require("uuid");
 
-const readVideos = () => {
-  const videosDataFile = fs.readFileSync("./data/videos.json");
+const readDataFile = (dataFilePath) => {
+  const videosDataFile = fs.readFileSync(dataFilePath);
   const videosData = JSON.parse(videosDataFile);
-  console.log(videosData);
   return videosData;
 };
 
+//Get Request
 router.get("/", (req, res) => {
-  const videosData = readVideos();
-  console.log("Data in get /video", videosData);
+  const videosData = readDataFile("./data/videos.json");
   res.status(200).json(videosData);
 });
+
+//Get requested video data from the video detail array
+router.get("/:videoId", (req, res) => {
+  const videoId = req.params.videoId;
+  const videoDetailsData = readDataFile("./data/video-detail.json");
+  const videoDetails = videoDetailsData.find((video) => video.id === videoId);
+
+  !videoDetails
+    ? res.status(404).send("Video not found")
+    : res.status(200).json(videoDetails);
+
+  return;
+});
+
+//POST request
 
 module.exports = router;
